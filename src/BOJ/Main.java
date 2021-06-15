46441633db76c910ada7b27190c65c2fae3b11bb
marinemaro <maro@marinero.kro.kr>
2021-06-15 14:29:54 +0900

    Update package name

--- src/BOJ/Main.java
+++ src/BOJ/Main.java
@@ -1,4 +1,4 @@
-package boj;
+package BOJ;
 
 public class Main {
     public static void main(String[] args) {
--- src/TEST/Main.java
+++ src/TEST/Main.java
@@ -1,4 +1,4 @@
-package test;
+package TEST;
 
 import java.io.*;
 import java.util.*;
