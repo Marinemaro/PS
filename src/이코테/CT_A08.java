a0167c7a151d4f658d7457b68eba3123cee3c310
marinemaro <maro@marinero.kro.kr>
2021-07-24 15:52:20 +0900

    이것이 코딩 테스트다 A-#8 : 문자열 재정렬

--- src/이코테/CT_A08.java
+++ src/이코테/CT_A08.java
@@ -10,16 +10,16 @@ public class CT_A08 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String in = br.readLine();
         char[] arr = in.toCharArray();
+
         Arrays.sort(arr);
+
         int sum = 0;
         boolean hasInteger = false;
-
         for(char c : arr)
             if(c >= '0' && c <= '9') {
                 sum+=c-'0';
                 hasInteger = true;
             } else System.out.print(c);
-
         if(hasInteger) System.out.println(sum);
     }
 }
