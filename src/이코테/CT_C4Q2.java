4356c65c0f5e991cd398aa9a169750c9a4be7f05
marinemaro <maro@marinero.kro.kr>
2021-07-24 16:10:51 +0900

    이것이 코딩 테스트다 C4-#2 : 시각

--- src/이코테/CT_C4Q2.java
+++ src/이코테/CT_C4Q2.java
@@ -8,13 +8,11 @@ public class CT_C4Q2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         int sum = 0;
         for(int i=0; i<=n; i++) {
             if(i%10==3) sum+=3600;
             else sum+=45*15+15*60;
         }
-
         System.out.println(sum);
     }
 }
