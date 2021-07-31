0a2a39ea96e3ce871e867a392c14827d69023dfd
marinemaro <maro@marinero.kro.kr>
2021-07-31 12:04:12 +0900

    이것이 코딩 테스트다 C6-#1 : 위에서 아래로

--- src/이코테/CT_C6Q1.java
+++ src/이코테/CT_C6Q1.java
@@ -1,10 +1,10 @@
-package TEST;
+package 이코테;
 
 import java.io.*;
 
 // 이것이 코딩 테스트다 챕터 6 - 1번째 문제 : 위에서 아래로
 
-public class Main {
+public class CT_C6Q1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
