d87e550d28fa587e18ac96c3935b2908a2c015ac
marinemaro <maro@marinero.kro.kr>
2021-08-03 01:27:01 +0900

    BOJ #2156: 포도주 시식

--- src/BOJ/BOJ_2156.java
+++ src/BOJ/BOJ_2156.java
@@ -1,8 +1,10 @@
-package TEST;
+package BOJ;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
+public class BOJ_2156 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
