c0768c3cb4ba1ebe5671de4dc7257923c096913e
marinemaro <maro@marinero.kro.kr>
2021-08-13 00:49:04 +0900

    BOJ #2012: 등수 매기기

--- src/BOJ/BOJ_2012.java
+++ src/BOJ/BOJ_2012.java
@@ -1,9 +1,18 @@
 package BOJ;
 
-import java.io.*;
-import java.util.*;
+/*
+문제 해결 아이디어
+1. 입력의 크기 n 만큼의 배열 생성
+2. 배열을 오름차순으로 정렬한 후 현재 등수(i값)와 예상 등수(a[i-1])의 절댓값을 변수 sum 에 저장
+3. sum 출력
+ */
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_2012 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
