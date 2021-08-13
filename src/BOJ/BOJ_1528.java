6f63fb33db775508657d4f4dc91caeda002fd8e4
marinemaro <maro@marinero.kro.kr>
2021-08-14 03:25:59 +0900

    BOJ #1528: 금민수의 합

--- src/BOJ/BOJ_1528.java
+++ src/BOJ/BOJ_1528.java
@@ -1,9 +1,24 @@
 package BOJ;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
-public class Main {
+/*
+문제 해결 아이디어
+1. 최대 입력값까지의 금민수 리스트를 구하는 전처리 과정 넣기
+2. 현재 입력값까지 금민수를 사용하여 표현할 수 있는지 DP를 통해 금민수의 최소 사용 개수 dp[n]을 구함
+3. 표현할 수 없다면, 즉 dp[n]값이 무한이라면 -1 출력, 표현할 수 있다면, 즉 dp[n]값이 무한이 아니라면 다음 단계 진행
+4. 어떤 금민수가 사용되었는지 dp 테이블에서 금민수 리스트에 저장된 수들을 통해 역추적
+   여기서 dp[n-t] 값이 무한이 아니라면, 금민수 t를 사용했다는 의미이다.
+   역추적 과정에서 가장 작은 값을 찾고, 작은 값이 여러개일 경우, 가장 작은 인덱스를 추적
+5. ans 리스트에 저장된 값을 정렬하고 출력하면 정답이다.
+ */
+
+public class BOJ_1528 {
     public static ArrayList<Integer> list = new ArrayList<>();
     public static void f(int i) {
         if(i>=1_000_000) return;
