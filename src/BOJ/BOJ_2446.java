ae4bc035de335b759a48ac9a6e69c8325c8429c0
marinemaro <maro@marinero.kro.kr>
2021-08-21 00:05:07 +0900

    BOJ #2446: 별 찍기 - 9

---
+++ src/BOJ/BOJ_2446.java
@@ -0,0 +1,29 @@
+package BOJ;
+
+import java.io.*;
+
+public class BOJ_2446 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in),16);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out),65536);
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n-1; i++) {
+            for(int j=0; j<i; j++) bw.write(' ');
+            for(int j=0; j<n*2-i*2-1; j++) bw.write('*');
+            bw.newLine();
+        }
+
+        for(int i=0; i<n-1; i++) bw.write(' ');
+        bw.write('*');
+        bw.newLine();
+
+        for(int i=0; i<n-1; i++) {
+            for(int j=0; j<n-2-i; j++) bw.write(' ');
+            for(int j=0; j<i*2+3; j++) bw.write('*');
+            bw.newLine();
+        }
+
+        bw.flush();
+    }
+}
