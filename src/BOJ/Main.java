0cbba346fe6878538a64a644f7df81be11aeea5c
marinemaro <maro@marinero.kro.kr>
2021-08-20 23:56:34 +0900

    BOJ #2446: 별 찍기 - 9

--- src/BOJ/Main.java
+++ src/BOJ/Main.java
@@ -1,41 +1,25 @@
 package BOJ;
 
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    final static int INF = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] in = br.readLine().split(" ");
-        int f = Integer.parseInt(in[0]);
-        int s = Integer.parseInt(in[1]);
-        int g = Integer.parseInt(in[2]);
-        int u = Integer.parseInt(in[3]);
-        int d = Integer.parseInt(in[4]);
+        int n = Integer.parseInt(br.readLine());
 
-        int[] map = new int[f+1];
-        Arrays.fill(map, INF);
-        Queue<Integer> q = new ArrayDeque<>();
-
-        q.add(s);
-        map[s] = 0;
-
-        while(!q.isEmpty()) {
-            int curr = q.poll();
-            if(curr == g) break;
-            int nu = curr+u;
-            int nd = curr-d;
-            if(nu <= f && map[nu]==INF) {
-                map[nu] = map[curr]+1;
-                q.add(nu);
-            }
-            if(nd > 0 && map[nd]==INF) {
-                map[nd] = map[curr]+1;
-                q.add(nd);
-            }
+        for(int i=0; i<n-1; i++) {
+            for(int j=0; j<i; j++) System.out.print(' ');
+            for(int j=0; j<n*2-i*2-1; j++) System.out.print('*');
+            System.out.println();
         }
 
-        System.out.println(map[g]==INF?"use the stairs":map[g]);
+        for(int i=0; i<n-1; i++) System.out.print(' ');
+        System.out.println('*');
+
+        for(int i=0; i<n-1; i++) {
+            for(int j=0; j<n-2-i; j++) System.out.print(' ');
+            for(int j=0; j<i*2+3; j++) System.out.print('*');
+            System.out.println();
+        }
     }
 }
