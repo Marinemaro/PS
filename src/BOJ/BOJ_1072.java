e0da2aeb67a4cc4d3e36176220b7910399b37fbd
marinemaro <maro@marinero.kro.kr>
2021-08-23 00:45:58 +0900

    BOJ #1072: 게임

--- src/BOJ/BOJ_1072.java
+++ src/BOJ/BOJ_1072.java
@@ -2,7 +2,15 @@ package BOJ;
 
 import java.io.*;
 
-public class Main {
+/*
+문제 해결 아이디어
+1. 전체 게임 횟수는 고정된 것이 아니다! 이걸 몰라서 오랫동안 헤맸다...
+2. 앞으로의 모든 게임에서 지지 않으므로, 처음 구한 승률에서 더 커진 승률의 경우(ex. 80% -> 81%)만 체크하면 된다.
+3. 추가 게임 횟수를 1부터 10억 사이로 두고, (이긴 게임+추가 게임)*100/(전체 게임+추가 게임)이 기존 승률에 비해
+   커지는지에 대해 이분탐색으로 탐색하다가 만약 조건이 맞다면 cnt 변수를 갱신하게끔 하면 cnt에 저장된 마지막 값이 정답이다.
+ */
+
+public class BOJ_1072 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] in = br.readLine().split(" ");
