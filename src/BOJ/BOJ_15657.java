abbe83d8e0610eafe9941bf0f04d85f02714fdc7
marinemaro <maro@marinero.kro.kr>
2021-08-25 17:43:14 +0900

    BOJ #15657: N과 M (8)

--- src/BOJ/BOJ_15657.java
+++ src/BOJ/BOJ_15657.java
@@ -3,7 +3,16 @@ package BOJ;
 import java.io.*;
 import java.util.Arrays;
 
-public class Main {
+/*
+문제 해결 아이디어
+1. 같은 수를 여러 번 고를 수 있기 때문에 visit[] 배열은 만들지 않는다.
+2. 깊이가 0(첫 호출) 이거나 이전에 저장된 수보다 크거나 같다면 '비내림차순' 조건에 해당하므로,
+   arr[] 배열에 현재 깊이를 인덱스로 삼고 현재 수를 저장한 후 깊이를 늘려가며 재귀호출을 한다.
+3. 만약 현재 깊이가 m에 도달했다면, arr[]에 저장된 모든 값을 출력하도록 한다.
+4. 이전에 저장된 수가 다음 반복 때 영향을 주지 않게끔 재귀호출을 빠져나왔다면 현재 깊이에 저장된 수를 0으로 초기화해준다.
+ */
+
+public class BOJ_15657 {
     static int n, m;
     static int[] a;
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
