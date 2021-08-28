0587d523276c3e07afec18b0b39a3e36b82d756e
marinemaro <maro@marinero.kro.kr>
2021-08-28 21:14:42 +0900

    BOJ #2470: 두 용액

--- src/BOJ/BOJ_2470.java
+++ src/BOJ/BOJ_2470.java
@@ -3,7 +3,14 @@ package BOJ;
 import java.io.*;
 import java.util.*;
 
-public class Main {
+/*
+문제 해결 아이디어 (투포인터) / 2467번과 매우 유사
+1. 입력이 정렬되지 않은 상태로 들어오므로 먼저 정렬을 해준 뒤, 양 쪽 끝에서부터 서로 만나기 전까지
+   음수라면 왼쪽 포인터 증가, 아니라면 오른쪽 포인터 감소로 O(NlogN) 시간 안에 답을 도출해낼 수 있다.
+2. 이전의 최적해와 현재 값을 비교하여 현재 값이 더 적다면 A와 B에 각 인덱스를 저장하고, 최적해를 갱신한다.
+ */
+
+public class BOJ_2470 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
