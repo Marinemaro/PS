f4ce05f156f658494b6f1691879572aa4ddeef9d
Marinemaro <maro@marinero.kro.kr>
2021-11-14 06:18:32 +0900

    BOJ #15810: 풍선 공장

--- src/BOJ/BOJ_15810.java
+++ src/BOJ/BOJ_15810.java
@@ -1,9 +1,8 @@
 package BOJ;
 
 import java.io.*;
-import java.util.*;
 
-public class Main {
+public class BOJ_15810{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] in = br.readLine().split(" ");
@@ -16,7 +15,6 @@ public class Main {
             a[i] = Integer.parseInt(in[i]);
             min = Math.min(a[i], min);
         }
-        Arrays.sort(a);
 
         long lo = min;
         long hi = (long)min*m;
@@ -25,9 +23,7 @@ public class Main {
         while(lo<=hi){
             long mid = (lo+hi)/2;
             long cnt = 0;
-            for(int i=0; i<n; i++) {
-                cnt += mid/a[i];
-            }
+            for(int i=0; i<n; i++) cnt += mid/a[i];
             if(cnt >= m) {
                 ans = Math.min(ans, mid);
                 hi = mid-1;
